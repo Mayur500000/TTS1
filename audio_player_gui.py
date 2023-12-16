30096777e5de63d73b833c8e5fb7dd70eafeba62
Mayur500000 <142081670+Mayur500000@users.noreply.github.com>
2023-12-16 20:39:47 +0530

    16/12

--- audio_player_gui.py
+++ audio_player_gui.py
@@ -1,16 +1,95 @@
-# audio_player_gui.py
-import pygame
+#audio_player_gui.py
+import os
+import tkinter as tk
+import pyttsx3
 
-def play_audio(audio_path):
-    print("Current working directory:", pygame.get_cwd())
-    pygame.mixer.init()
-    pygame.mixer.music.load(audio_path)
-    pygame.mixer.music.play()
+class MP3PlayerLogic:
+    def __init__(self, mp3_paths):
+        self.mp3_paths = mp3_paths
+        self.selected_index = 0
 
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
+    def get_selected_mp3(self):
+        return self.mp3_paths[self.selected_index]
+
+    def select_previous_mp3(self):
+        if self.selected_index > 0:
+            self.selected_index -= 1
+
+    def select_next_mp3(self):
+        if self.selected_index < len(self.mp3_paths) - 1:
+            self.selected_index += 1
+
+    def play_selected_mp3(self):
+        selected_mp3 = self.get_selected_mp3()
+        return f"Now playing: {os.path.basename(selected_mp3)}"
+
+class MP3PlayerGUI:
+    def __init__(self, master, mp3_paths):
+        self.master = master
+        master.title("MP3 Player")
+        master.bind("<Control-t>", self.show_instructions)
+        master.bind("<Return>", self.play_selected_mp3)
+        master.bind("<Up>", self.select_previous_mp3)
+        master.bind("<Down>", self.select_next_mp3)
+
+        self.logic = MP3PlayerLogic(mp3_paths)
+
+        self.mp3_label = tk.Label(master, text="")
+        self.mp3_label.grid(row=1, column=0, columnspan=3, pady=10)
+
+        self.show_instructions()
+
+    def show_instructions(self, event=None):
+        instructions = (
+            "Welcome to the MP3 Player!\n\n"
+            "Use the arrow keys to navigate and press Enter to play an MP3 file.\n"
+            "Press Ctrl+Q to quit the application."
+        )
+
+        self.speak(instructions)
+
+        self.show_mp3_selection()
+
+    def show_mp3_selection(self):
+        selected_mp3 = self.logic.get_selected_mp3()
+        message = f"Selected MP3: {os.path.basename(selected_mp3)}"
+        self.mp3_label.config(text=message)
+        self.speak(message)
+
+    def select_previous_mp3(self, event=None):
+        self.logic.select_previous_mp3()
+        self.show_mp3_selection()
+
+    def select_next_mp3(self, event=None):
+        self.logic.select_next_mp3()
+        self.show_mp3_selection()
+
+    def play_selected_mp3(self, event=None):
+        result_message = self.logic.play_selected_mp3()
+        self.speak(result_message)
+        self.show_mp3_selection()
+
+        # Open the MP3 file with the default media player
+        os.startfile(self.logic.get_selected_mp3())
+
+    def speak(self, text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
 
 if __name__ == "__main__":
-    # Example usage
-    audio_file_path = "output_DSCC.mp3"  # Update with the actual output file path
-    play_audio(audio_file_path)
+    # Get the path to the script's directory
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # List all files in the script's directory
+    all_files = os.listdir(script_directory)
+
+    # Filter only the files with the .mp3 extension
+    mp3_files = [file for file in all_files if file.endswith('.mp3')]
+
+    # Create the mp3_paths list
+    mp3_paths = [os.path.join(script_directory, mp3_file) for mp3_file in mp3_files]
+
+    root = tk.Tk()
+    app = MP3PlayerGUI(root, mp3_paths)
+    root.mainloop()
--- guiTTStest1Gui.py
+++ guiTTStest1Gui.py
@@ -3,17 +3,20 @@ import tkinter as tk
 import os
 import pyttsx3
 from guiTTStest1Logic import PDFConverterLogic
+from audio_player_gui import MP3PlayerGUI
 
 class PDFToAudioConverterGUI:
-    def __init__(self, master, pdf_paths):
+    def __init__(self, master, pdf_paths, mp3_paths):
         self.master = master
         master.title("PDF to Audiobook Converter")
-        master.bind("<Control-t>", self.show_instructions)
+        master.bind("<Control-t>", self.switch_to_pdf_converter)
+        master.bind("<Control-a>", self.switch_to_audio_player)
         master.bind("<Return>", self.convert_selected_pdf)
         master.bind("<Up>", self.select_previous_pdf)
         master.bind("<Down>", self.select_next_pdf)
 
         self.logic = PDFConverterLogic(pdf_paths)
+        self.mp3_paths = mp3_paths
 
         self.pdf_label = tk.Label(master, text="")
         self.pdf_label.grid(row=1, column=0, columnspan=3, pady=10)
@@ -24,10 +27,15 @@ class PDFToAudioConverterGUI:
         instructions = (
             "Welcome to the PDF to Audiobook Converter!\n\n"
             "Use the arrow keys to navigate and press Enter to select a PDF.\n"
-            "Press Ctrl+Q to quit the application."
+            "Press Ctrl+Q to quit the application.\n"
+            "Press Ctrl+A to open the Audio Player."
         )
 
         self.speak(instructions)
+
+        # Show additional message for Ctrl+A
+        audio_instructions = "Press Ctrl+A to listen to the selected audio."
+        self.speak(audio_instructions)
 
         self.show_pdf_selection()
 
@@ -55,12 +63,32 @@ class PDFToAudioConverterGUI:
         engine.say(text)
         engine.runAndWait()
 
+    def switch_to_audio_player(self, event=None):
+    # Open the audio player as a child window
+       audio_player_window = tk.Toplevel(self.master)
+       print("MP3 Paths:", self.mp3_paths)  # Debugging line
+       audio_player_app = MP3PlayerGUI(audio_player_window, self.mp3_paths)
+
+
+    def switch_to_pdf_converter(self, event=None):
+        # Close the audio player window
+        for window in self.master.winfo_children():
+            if isinstance(window, tk.Toplevel):
+                window.destroy()
+
 if __name__ == "__main__":
+    # Get the path to the script's directory
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # List all files in the script's directory
+    all_files = os.listdir(script_directory)
+
+    # Filter only the files with the .pdf extension
+    pdf_files = [file for file in all_files if file.endswith('.pdf')]
+
+    # Create the pdf_paths list
+    pdf_paths = [os.path.join(script_directory, pdf_file) for pdf_file in pdf_files]
+
     root = tk.Tk()
-    pdf_paths = [
-        'D:/Mayur/code/TtS/TTS1/DSCC.pdf',
-        'D:/Mayur/code/TtS/TTS1/test1.pdf',
-        'D:/Mayur/code/TtS/TTS1/EH.pdf'
-    ]
-    app = PDFToAudioConverterGUI(root, pdf_paths)
+    app = PDFToAudioConverterGUI(root, pdf_paths, [])
     root.mainloop()
